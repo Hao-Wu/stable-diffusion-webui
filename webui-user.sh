d500bf18b6a7efa7796d1d7f4f8462ef33b91905
Hao-Wu <haowu.wuhao@gmail.com>
2023-07-05 19:37:38 +0800

    add webui-user args with testing on macbook pro M1 and M2 cpu

--- webui-user.sh
+++ webui-user.sh
@@ -10,7 +10,10 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-export COMMANDLINE_ARGS="--skip-torch-cuda-test --upcast-sampling --opt-sub-quad-attention --use-cpu interrogate"
+export COMMANDLINE_ARGS="--skip-torch-cuda-test --upcast-sampling --no-half-vae --use-cpu interrogate"
+# the following COMMANDLINE_ARGS can also be applied for different hardware configurations
+# export COMMANDLINE_ARGS="--skip-torch-cuda-test --opt-sub-quad-attention --upcast-sampling --no-half --use-cpu interrogate"
+# export COMMANDLINE_ARGS="--opt-sub-quad-attention --disable-safe-unpickle --skip-torch-cuda-test --medvram --upcast-sampling --no-half --no-half-vae --use-cpu interrogate"
 
 # python3 executable
 #python_cmd="python3"
@@ -25,7 +28,7 @@ venv_dir="venv-torch-nightly"
 #export LAUNCH_SCRIPT="launch.py"
 
 # install command for torch
-export TORCH_COMMAND="pip install --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html"
+export TORCH_COMMAND="pip install --pre torch==2.1.0.dev20230704 torchvision==0.16.0.dev20230704 -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html"
 
 # Requirements file to use for stable-diffusion-webui
 #export REQS_FILE="requirements_versions.txt"
